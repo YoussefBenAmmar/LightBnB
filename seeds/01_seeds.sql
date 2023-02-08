f8577825f8845cfcb175c5e9401175313b0604ef
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-08 15:35:39 -0500

    download lightbnb webapp

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO users (name, email, password)
+VALUES ('Khabib Nurmagomedov', 'smesh@ufc.dead', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Francis Ngannou', 'punch@free.strong', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Kamaru Usman', 'come@back.always', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+
+
+INSERT INTO properties ( title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES ('List least', 'Young trouble skin outline pattern energy', 391, 'https://images.pexels.com/photos/2099019/pexels-photo-2099019.jpeg', 'https://images.pexels.com/photos/2099019/pexels-photo-2099019.jpeg?auto=compress&cs=tinysrgb&h=350', 34565, 0, 1, 1, true, 'Newfoundland And Labrador', 'Paradise', 'Canada', '1848 Cuzo Trail', '08409')
+    INSERT INTO properties (title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES ('List least', 'Young trouble skin outline pattern energy', 391, 'https://images.pexels.com/photos/2099019/pexels-photo-2099019.jpeg', 'https://images.pexels.com/photos/2099019/pexels-photo-2099019.jpeg?auto=compress&cs=tinysrgb&h=350', 34565, 0, 1, 1, true, 'Newfoundland And Labrador', 'Paradise', 'Canada', '1848 Cuzo Trail', '08409')
+
+
+
+
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 1, 2),
+('2021-10-01', '2021-10-14', 1, 1);
+
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES ( 891, 290, 1016559, 3, 'Hawapel pasale udmupnej co uzfos novuhig bilad dedgure vim renfef soumi suti bop.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES ( 890, 290, 1016559, 3, 'Hawapel pasale udmupnej co uzfos novuhig bilad dedgure vim renfef soumi suti bop.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES ( 811, 290, 1016559, 3, 'Hawapel pasale udmupnej co uzfos novuhig bilad dedgure vim renfef soumi suti bop.');
