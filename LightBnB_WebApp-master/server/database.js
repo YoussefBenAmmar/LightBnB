cbdbb59323aa378cdc6bf1706c8d3e49ab50a513
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-09 09:34:46 -0500

    refactor reservation funciton on js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -115,6 +115,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
+  const strQuery = `
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+
+  const values = [guest_id, limit]
+
+  return pool 
+    .query(strQuery, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return ('error', err);
+    });
+
   return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
